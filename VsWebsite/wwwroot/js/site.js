e3f83ed838005b3c6f6f0df71dc8ef36461d0230
Gage-H-12 <gage.hayes13@gmail.com>
2024-10-25 05:18:38 -0700

    Add project files.

---
+++ VsWebsite/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿let dvdLogo = document.getElementById("music");
+const header = document.querySelector('header');
+const footer = document.querySelector('footer');
+
+loop();
+function loop() {
+    const headerHeight = header.clientHeight;
+    const footerHeight = footer.clientHeight;
+
+
+    const hRange = window.innerWidth - dvdLogo.clientWidth;
+    const vRange = window.innerHeight - dvdLogo.clientHeight - headerHeight - footerHeight;
+
+    const time = performance.now() * 0.2;
+
+    const x = Math.abs((time % (hRange * 2)) - hRange);
+    dvdLogo.style.left = `${x}px`;
+
+    const y = Math.abs((time % (vRange * 2)) - vRange);
+    dvdLogo.style.top = `${y}px`;
+
+    requestAnimationFrame(loop);
+}
